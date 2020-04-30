8ed548eaf4ef40c4b7ff3cdf54f85a1d5e701d05
Ramon <ramonbhb@gmail.com>
2020-04-29 21:25:53 -0300

    Serviços

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EventosService } from './eventos.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [EventosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
